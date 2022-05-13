0d4913acdfb8422c6bed0eafa7fe9cf97947f860
jinwoo <wwo1278@gmail.com>
2022-05-13 23:52:04 +0900

    Breadcrumb 생성

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import Loading from './Loading.js';
 import { request } from './api.js';
 import Breadcrumb from './Breadcrumb.js';
 
+const cache = {};
+
 export default function App($app) {
   this.state = {
     isRoot: false,
@@ -14,7 +16,37 @@ export default function App($app) {
   this.setState = (nextState) => {
     this.state = nextState;
     loading.setState(this.state.isLoading);
+    breadcrumb.setState(this.state.depth);
   };
+
+  const breadcrumb = new Breadcrumb({
+    $app,
+    initialState: this.state.depth,
+    onClick: (index) => {
+      if (index === null) {
+        this.setState({
+          ...this.state,
+          depth: [],
+          isRoot: true,
+          nodes: cache.rootNodes,
+        });
+        return;
+      }
+
+      if (index === this.state.depth.length - 1) {
+        console.log('여긴가');
+        return;
+      }
+      const nextState = { ...this.state };
+      const nextDepth = this.state.depth.slice(0, index + 1);
+
+      this.setState({
+        ...nextState,
+        depth: nextDepth,
+        nodes: cache[nextDepth[nextDepth.length - 1].id],
+      });
+    },
+  });
 
   const loading = new Loading({
     $app,
@@ -29,12 +61,14 @@ export default function App($app) {
         isLoading: true,
       });
       const rootNodes = await request();
-      console.log(rootNodes);
       this.setState({
         ...this.state,
         isRoot: true,
         nodes: rootNodes,
       });
+
+      cache.rootNodes = rootNodes;
+      console.log(cache.rootNodes);
     } catch (error) {
       console.log(error);
     } finally {
