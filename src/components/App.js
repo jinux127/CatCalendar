cd73746042d5263c503f71b8e60296066cfa2320
jinwoo <wwo1278@gmail.com>
2022-05-14 00:33:05 +0900

    노드 생성!!

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Loading from './Loading.js';
 import { request } from './api.js';
 import Breadcrumb from './Breadcrumb.js';
+import Nodes from './Nodes.js';
 
 const cache = {};
 
@@ -17,6 +18,10 @@ export default function App($app) {
     this.state = nextState;
     loading.setState(this.state.isLoading);
     breadcrumb.setState(this.state.depth);
+    nodes.setState({
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    });
   };
 
   const breadcrumb = new Breadcrumb({
@@ -34,7 +39,6 @@ export default function App($app) {
       }
 
       if (index === this.state.depth.length - 1) {
-        console.log('여긴가');
         return;
       }
       const nextState = { ...this.state };
@@ -48,6 +52,77 @@ export default function App($app) {
     },
   });
 
+  const nodes = new Nodes({
+    $app,
+    initialState: [],
+    onClick: async (node) => {
+      console.log(node);
+
+      try {
+        this.setState({
+          ...this.state,
+          isLoading: true,
+        });
+        if (node.type === 'DIRECTORY') {
+          if (cache[node.id]) {
+            this.setState({
+              ...this.state,
+              isRoot: false,
+              depth: [...this.state.depth, node],
+              nodes: cache[node.id],
+              isLoading: false,
+            });
+          } else {
+            const nextNodes = await request(node.id);
+            this.setState({
+              ...this.state,
+              isRoot: false,
+              depth: [...this.state.depth, node],
+              nodes: nextNodes,
+              isLoading: false,
+            });
+
+            cache[node.id] = nextNodes;
+          }
+        } else if (node.type === 'FILE') {
+          this.setState({
+            ...this.state,
+            isRoot: false,
+            selectedFilePath: node.filePath,
+            isLoading: false,
+          });
+        }
+      } catch (e) {
+        console.log(`노드 클릭 에러 :${e.message}`);
+      }
+    },
+    onBackClick: async () => {
+      try {
+        const nextState = { ...this.state };
+        nextState.depth.pop();
+
+        const prevNodeId = nextState.depth.length === 0 ? null : nextState.depth[nextState.depth.length - 1].id;
+
+        this.setState({
+          ...nextState,
+        });
+
+        if (prevNodeId === null) {
+          this.setState({
+            ...nextState,
+            isRoot: true,
+            nodes: cache.rootNodes,
+          });
+        } else {
+          this.setState({
+            ...nextState,
+            isRoot: false,
+            nodes: cache[prevNodeId],
+          });
+        }
+      } catch (error) {}
+    },
+  });
   const loading = new Loading({
     $app,
     initialState: this.state.isLoading,
@@ -68,7 +143,6 @@ export default function App($app) {
       });
 
       cache.rootNodes = rootNodes;
-      console.log(cache.rootNodes);
     } catch (error) {
       console.log(error);
     } finally {
